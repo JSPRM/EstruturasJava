fead756996dc831088c07953226659f4fb1f4746
Jose Alberto Sanchez Pacheco <jasp@ibm.com>
2022-10-03 22:04:21 -0500

    Linked list

---
+++ src/LinkedListJava.java
@@ -0,0 +1,21 @@
+public class LinkedListJava {
+    NodeLink head;
+
+    static class NodeLink {
+        int value;
+        NodeLink next;
+
+        NodeLink(int d){
+            value = d;
+            next = null;
+        }
+    }
+
+    public void print(){
+        NodeLink n = head;
+        while (n != null){
+            System.out.println(n.value + " ");
+            n = n.next;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LinkedListJava llist = new LinkedListJava();
+
+        llist.head = new LinkedListJava.NodeLink(1);
+        LinkedListJava.NodeLink second = new LinkedListJava.NodeLink(2);
+        LinkedListJava.NodeLink tercero = new LinkedListJava.NodeLink(3);
+
+        llist.head.next = second;
+        second.next = tercero;
+
+        llist.print();
     }
 }
